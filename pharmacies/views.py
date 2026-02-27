1859429761524fabe4a5a14064b63636c8bb8da2
joe-qodes <jquao04@gmail.com>
2026-02-27 16:49:30 +0000

    added urls.py and implemented views for pharmacies app

--- pharmacies/views.py
+++ pharmacies/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Pharmacy
+from .serializers import PharmacySerializer
 
-# Create your views here.
+
+class PharmacyViewSet(viewsets.ModelViewSet):
+    queryset = Pharmacy.objects.all()
+    serializer_class = PharmacySerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
