6b1e036f451be9bb96b6760643bf40934b9d217a
joe-qodes <jquao04@gmail.com>
2026-02-27 16:46:24 +0000

    Implemented models and views for pharmacies app

--- pharmacies/models.py
+++ pharmacies/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from accounts.models import User
 
-# Create your models here.
+
+class Pharmacy(models.Model):
+
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name="pharmacy_profile"
+    )
+
+    name = models.CharField(max_length=255)
+    license_number = models.CharField(max_length=100)
+    address = models.TextField()
+    is_verified = models.BooleanField(default=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
---
+++ pharmacies/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from .models import Pharmacy
+
+
+class PharmacySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Pharmacy
+        fields = '__all__'
+        read_only_fields = ['user']
