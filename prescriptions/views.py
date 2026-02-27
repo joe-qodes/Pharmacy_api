d453e8b7df11d5e4a175b17be3b858678dd71112
joe-qodes <jquao04@gmail.com>
2026-02-27 17:13:15 +0000

    added urls.py and implemented views for precriptions

--- prescriptions/views.py
+++ prescriptions/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Prescription
+from .serializers import PrescriptionSerializer
 
-# Create your views here.
+
+class PrescriptionViewSet(viewsets.ModelViewSet):
+    queryset = Prescription.objects.all()
+    serializer_class = PrescriptionSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(patient=self.request.user)
+
+    def get_queryset(self):
+        user = self.request.user
+
+        if user.role == 'admin':
+            return Prescription.objects.all()
+
+        return Prescription.objects.filter(patient=user)
