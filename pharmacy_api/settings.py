02a36f9a3ae2a8c47dd78e7d7b27091bf2241094
joe-qodes <jquao04@gmail.com>
2026-02-27 16:12:18 +0000

    cleaned some files to fix an issue

--- pharmacy_api/settings.py
+++ pharmacy_api/settings.py
@@ -38,12 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core',
-    'medicines',
-    'orders',
-    'pharmacies',
-    'prescriptions',
-    'users'
 ]
 
 MIDDLEWARE = [
