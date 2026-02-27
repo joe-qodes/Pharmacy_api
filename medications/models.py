6519986975508c9590ba0a5ecc3217625aba3ef7
joe-qodes <jquao04@gmail.com>
2026-02-27 16:59:31 +0000

    implemented the models for medications and include jwt in the project settings

--- medications/models.py
+++ medications/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from pharmacies.models import Pharmacy
 
-# Create your models here.
+
+class Medication(models.Model):
+    name = models.CharField(max_length=255)
+    category = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class PharmacyMedication(models.Model):
+
+    pharmacy = models.ForeignKey(
+        Pharmacy,
+        on_delete=models.CASCADE,
+        related_name="inventory"
+    )
+
+    medication = models.ForeignKey(
+        Medication,
+        on_delete=models.CASCADE
+    )
+
+    stock = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.medication.name} - {self.pharmacy.name}"
