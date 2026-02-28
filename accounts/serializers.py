d9e5a0203abd081d4d94656d0df4c6eab16e5720
joe-qodes <jquao04@gmail.com>
2026-02-28 11:45:35 +0000

    implemented register, login and logout features with skeleton sites

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -12,7 +12,12 @@ class RegisterSerializer(serializers.ModelSerializer):
         fields = ['id', 'username', 'email', 'password', 'role']
 
     def create(self, validated_data):
-        user = User.objects.create_user(**validated_data)
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password'],
+            role=validated_data['role']
+        )
         return user
 
 
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,10 @@
 from rest_framework import viewsets, permissions
 from django.contrib.auth import get_user_model
 from .serializers import RegisterSerializer, UserSerializer
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as django_login
+from django.contrib.auth import logout
 
 User = get_user_model()
 
@@ -17,3 +21,43 @@ class UserViewSet(viewsets.ModelViewSet):
         if self.action == 'create':
             return [permissions.AllowAny()]
         return [permissions.IsAuthenticated()]
+    
+
+def home(request):
+    return render(request, 'home.html')
+
+def register_page(request):
+    if request.method == 'POST':
+        User.objects.create_user(
+            username=request.POST['username'],
+            email=request.POST['email'],
+            password=request.POST['password'],
+            role=request.POST['role']
+        )
+        return redirect('login_page')
+
+    return render(request, 'register.html')
+
+
+def login_page(request):
+    if request.method == 'POST':
+        user = authenticate(
+            username=request.POST['username'],
+            password=request.POST['password']
+        )
+        if user:
+            django_login(request, user)
+            return redirect('dashboard')
+
+    return render(request, 'login.html')
+
+def dashboard(request):
+    if not request.user.is_authenticated:
+        return redirect('login_page')
+
+    return render(request, 'dashboard.html')
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('home')
