fecdba146a60d24b716a58342c99568514889eda
joe-qodes <jquao04@gmail.com>
2026-03-01 00:57:28 +0000

    modified serializers of pharmacies to implement prescription upload feature

--- pharmacies/serializers.py
+++ pharmacies/serializers.py
@@ -3,8 +3,10 @@ from .models import Pharmacy
 
 
 class PharmacySerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(source='user.email', read_only=True)
+    phone_number = serializers.CharField(source='user.phone_number', read_only=True)
 
     class Meta:
         model = Pharmacy
-        fields = '__all__'
-        read_only_fields = ['user']
+        fields = ['id', 'name', 'license_number', 'address', 'is_verified', 'created_at', 'email', 'phone_number']
+        read_only_fields = ['user', 'is_verified']
